3d8dbc10277a00f8cb7cfac349fcc72167559745
Seyi Afolayan <oafolay2@jh.edu>
2024-07-16 06:55:01 -0400

    updated

--- asbr_package/asbr_test_ws/build/asbr_description/colcon_command_prefix_build.sh
+++ asbr_package/asbr_test_ws/build/asbr_description/colcon_command_prefix_build.sh
@@ -1,1 +1,2 @@
 # generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/srobot/ASBR_demo_integration/asbr_package/asbr_test_ws/install/ur_description/share/ur_description/package.sh"
---
+++ asbr_package/asbr_test_ws/build/realsense2_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/srobot/ASBR_demo_integration/asbr_package/asbr_test_ws/src/realsense_description/realsense2_description
+# Build directory: /home/srobot/ASBR_demo_integration/asbr_package/asbr_test_ws/build/realsense2_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
