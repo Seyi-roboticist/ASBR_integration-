e1d5bb4d187d3677f54874fe7c593adc3c3df284
Seyi R. Afolayan <143431845+Seyi-roboticist@users.noreply.github.com>
2024-07-24 21:53:56 -0400

    Delete asbr_package/asbr_test_ws/build directory

--- asbr_package/asbr_test_ws/build/robotiq_2f_85_gripper_visualization/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/srobot/ASBR_integration-/asbr_package/asbr_test_ws/src/robotiq_description/robotiq_2f_85_gripper_visualization
-# Build directory: /home/srobot/ASBR_integration-/asbr_package/asbr_test_ws/build/robotiq_2f_85_gripper_visualization
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- asbr_package/asbr_test_ws/build/ur5e_cobot_bringup/colcon_command_prefix_build.sh
+++
@@ -1,4 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/srobot/ASBR_integration-/asbr_package/asbr_test_ws/install/realsense2_description/share/realsense2_description/package.sh"
-. "/home/srobot/ASBR_integration-/asbr_package/asbr_test_ws/install/ur_description/share/ur_description/package.sh"
-. "/home/srobot/ASBR_integration-/asbr_package/asbr_test_ws/install/asbr_description/share/asbr_description/package.sh"
